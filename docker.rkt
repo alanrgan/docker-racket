2428a52c86387ed4c3f954e69661629300398265
Alan Gan <alanrgan@gmail.com>
2017-06-18 15:15:49 -0400

    Added more container methods: start, stop, restart, rename

--- docker.rkt
+++ docker.rkt
@@ -9,6 +9,9 @@
          containers/ls
          call-with-command)
 
+(define (any->string x)
+  (format "~a" x))
+
 (define (string->bytes str)
   (let ([port (open-output-bytes)])
     (write str port)
@@ -16,6 +19,16 @@
 
 (define (words str)
   (string-split str " "))
+
+(define (concat-strs lst)
+  (foldl (lambda (s acc) (string-append acc s) "" lst)))
+
+(define (join-pair with pair)
+  (if (null? pair)
+      '()
+      (string-append (car pair)
+                     with
+                     (any->string (cadr pair)))))
 
 ;; Right now only Unix systems supported
 
@@ -32,9 +45,9 @@
                       0))]
          [status-code (list-ref status 1)]
          [spl (string-split response "\r\n\r\n")]
-         [body (if (< (length spl) 3)
+         [body (if (>= (length spl) 2)
                    (list-ref spl 1)
-                   '())])
+                   "")])
     (cons status-code body)))
 
 (define (make-request-str type str)
@@ -57,11 +70,24 @@
             docker-sock))
   (let ([response
 		      (http-parse
-				  (with-output-to-string
-				    (lambda () (system request))))])
-    (cons (car response) 
+  				  (with-output-to-string
+  				    (lambda () (system request))))])
+    (cons (car response)
 		      (string->jsexpr
-		      (cdr response)))))
+		        (cdr response)))))
+
+(define build-command
+  (lambda (domain args #:query [query '()])
+    (define base-command
+      (string-append "/"
+        (string-join (cons domain args) "/")))
+    (let ([query (map (curry join-pair "=") query)])
+      (cond ((null? query) base-command)
+            (else
+              (string-append
+                base-command
+                "?"
+                (string-join query "&")))))))
 
 (define (call-with-command type command)
   (docker-call
@@ -77,4 +103,33 @@
     (define all (if allv 1 0))
   	(call-with-command
      	'GET
-      (format "/containers/json?all=~a" all))))
+      (build-command "containers"
+                     '("json")
+        #:query `(["all" ,all])))))
+
+(define (containers/start name)
+  (let ([command (build-command "containers"
+                    #:query `(,name "start"))])
+    (call-with-command 'POST command)))
+
+(define containers/stop
+  (lambda (name #:wait-time [wait-time 0])
+    (let ([command (build-command "containers"
+                                  `(,name "stop")
+                     #:query `(["t" ,wait-time]))])
+      (call-with-command 'POST command))))
+
+(define containers/restart
+  (lambda (name #:wait-time [wait-time 0])
+    (when (number? wait-time)
+          (set! wait-time (number->string wait-time)))
+    (let ([command (build-command "containers"
+                                  `(,name "restart")
+                      #:query `(["t" ,wait-time]))])
+      (call-with-command 'POST command))))
+
+(define (containers/rename name new-name)
+  (let ([command (format "/containers/~a/rename?name=~a"
+                         name
+                         new-name)])
+    (call-with-command 'POST command)))
